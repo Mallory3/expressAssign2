d062bc6ab7e5b3935fce7225a5d4f94a7da37f85
Mallory3 <55147157+Mallory3@users.noreply.github.com>
2019-11-01 19:57:44 -0600

    added assignment 2

--- node-express-mongodb-master/src/assignment-2/app.js
+++ node-express-mongodb-master/src/assignment-2/app.js
@@ -4,17 +4,25 @@ const express = require('express');
 const path = require('path');
 //require pageInfo
 const pageInfo = require('./pageInfo');
+//require middleware
+const middleware = require('./middleware');
 //envoke express
 const app = express();
 
-// allows us to exclude the file extension
+//Made and included favicon
+const favicon = require('express-favicon');
+app.use(favicon(__dirname + '/assets/img/gem.ico/favicon.ico'));
+
+
+//exclude .ejs from url
 app.set('view engine','ejs'); 
+
+app.use(express.urlencoded({ extended: false }));
 
 
 //use app.get to render the .ejs
 //required pageInfo to loop through values in the pageInfo module in the header paritial so that the correct title and css pages were linked
 app.get('/:page', function(request, response){
- 
   response.render(request.params.page,pageInfo[request.params.page]);
 })
 
@@ -22,14 +30,18 @@ app.get('/', function(request, response){
   response.render('index',pageInfo.index);
 })
 
-//creating a JSON array of members
+//evoking middleware module to log name:email in log.txt when form is submitted
+app.use(middleware)
 
 
+//use app.post to redirect users to /submit upon submitting form in footer
+app.post('/submit', function(request,response) {
+  response.render('submit', {data: request.body})
+  console.log('updated')
+});
 
 //set assets as a static folder so it renders all the files within it
 app.use(express.static(path.join(__dirname, 'assets')));
-
-
 
 //create a variable with a PORT number. (process.env.port looks at the environment variables for PORT or PORT 3000)
 const PORT = process.env.PORT || 3000;
